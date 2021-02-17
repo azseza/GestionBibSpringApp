2b2ccfcf8f93c7580ae9c93e4dc673db5744029f
azseza <azeer.ltifi@gmail.com>
2021-02-17 17:35:08 +0100

    all html front pages complete

--- src/main/java/com/ams/biblio/controllers/BookController.java
+++ src/main/java/com/ams/biblio/controllers/BookController.java
@@ -3,10 +3,17 @@ package com.ams.biblio.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ams.biblio.repositories.BookRepository;
+import com.ams.biblio.repositories.CategoryRepository;
+
 @Controller
 //add a paramater as the cathegory id 
 @RequestMapping("/allBooks")
 public class BookController {
-	//view all books of cathegory 
+	private final BookRepository bookRepository;
+	private final CategoryRepository categoryRepository;
+	
+	
+	
 
 }
